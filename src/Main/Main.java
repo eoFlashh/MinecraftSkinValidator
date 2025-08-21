2550d37dc3d9ab957dfbb73f00177b8f00128e98
eoFlashh <mailrflashh@gmail.com>
2025-08-21 19:22:44 -0300

    It is now possible to send a path with multiple files

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,37 +5,56 @@ import Models.Face;
 import Models.Faces;
 import Models.Pixel;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the file path: ");
+        System.out.println("Enter the file path or path: ");
         String path = sc.nextLine();
         sc.close();
-        Analise analise = new Analise();
-        List<Pixel> wrongPixels;
-        List<Pixel> skin;
+        File file = new File(path);
 
-        try {
-            if (!path.endsWith(".png")) {
-                System.out.println("Invalid image.");
-            } else {
-                wrongPixels = analise.wrongPixels(path);
-                skin = analise.loadSkin(path);
-                String skinTypeMap = analise.identifyFacesTipe(path);
-                Faces faces = new Faces();
-                System.out.println(skinTypeMap);
-
-                HashMap<String, String> listFaces = faces.listFaces();
-                List<Face> mapFaces = faces.mapsFaces(skinTypeMap, listFaces);
-                List<String> errors = analise.verdict(wrongPixels, skin, mapFaces);
-                for (int i = 0; i < errors.size(); i++) {
-                    System.out.println(errors.get(i));
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    System.out.println(f.getName());
+                    if (f.isFile() && f.getName().toLowerCase().endsWith(".png")) {
+                        analiseVeredict(f.getAbsolutePath());
+                    } else {
+                        System.out.println("Invalid image.");
+                    }
+                    System.out.println(" ");
                 }
             }
+        } else if (!path.toLowerCase().endsWith(".png")) {
+            System.out.println("Invalid image.");
+        } else {
+            analiseVeredict(path);
+        }
+    }
+
+    private static void analiseVeredict(String path) {
+        try {
+            Analise analise = new Analise();
+            List<Pixel> wrongPixels = analise.wrongPixels(path);
+            List<Pixel> skin = analise.loadSkin(path);
+            String skinTypeMap = analise.identifyFacesTipe(path);
+
+            Faces faces = new Faces();
+            System.out.println(skinTypeMap);
+
+            HashMap<String, String> listFaces = faces.listFaces();
+            List<Face> mapFaces = faces.mapsFaces(skinTypeMap, listFaces);
+            List<String> errors = analise.verdict(wrongPixels, skin, mapFaces);
+            for (String error : errors) {
+                System.out.println("   " + error);
+            }
+
         } catch (Exception e) {
             System.out.println("Invalid image.");
         }
